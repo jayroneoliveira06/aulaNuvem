f77db71cb00d9cdb984349afa271204a614cbc42
aluno <e023aluno@outlook.com>
2026-03-27 17:50:18 -0300

    jarone

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', (req, res)=>{
     res.send("Servidor rodando na nuvem 🚀");
 
 })
-
+app.get('/teste', (req, res)=>{
+    res.send("Rota de teste funcionando!");
+})
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
